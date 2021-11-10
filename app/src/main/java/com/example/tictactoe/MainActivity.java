d81db2c8837d28aa472cf9488227c11a398e44de
Ryder <rdeback@gmail.com>
2021-11-10 11:02:49 -0800

    Finished all code on game and got it fully working.

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -25,6 +25,12 @@ public class MainActivity extends AppCompatActivity
 
     private String player;
     private TextView currTurn;
+    //Helps keep track of when a win condition should be triggered.
+    private int count;
+    //Removes the magical == 9 from down below
+    //Basically if the count ever hits 9 that means a player has 5 and the other has 4 without
+    //any win conditions coming through.
+    private final  int tie = 9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,7 +38,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //Establishing all of the buttons and the button array that I use later.
         buttonTL = findViewById(R.id.buttonTL);
         buttonTM = findViewById(R.id.buttonTM);
         buttonTR = findViewById(R.id.buttonTR);
@@ -45,7 +51,9 @@ public class MainActivity extends AppCompatActivity
         BUTTONS = new Button[] {buttonTL,buttonTM,buttonTR,buttonML,buttonMM,buttonMR,buttonBL,buttonBM,buttonBR};
         newGameButton = findViewById(R.id.newGameButton);
 
+        //Setting default values of player count and the starting players turn.
         player = "X";
+        count = 0;
         currTurn = findViewById(R.id.playerTurnTextView);
 
         resetGame();
@@ -54,6 +62,8 @@ public class MainActivity extends AppCompatActivity
     private void resetGame()
     {
         //Reduced down redundant code and made it into a for loop.
+        //That sets all the buttons to being re-enabled and blank text boxes
+        //when the NEW GAME button is pushed.
         for(int i = 0; i < BUTTONS.length; i++)
         {
             BUTTONS[i].setText("");
@@ -61,6 +71,7 @@ public class MainActivity extends AppCompatActivity
         }
 
         player = "X";
+        count = 0;
         currTurn.setText("Player X's turn");
     }
 
@@ -79,26 +90,87 @@ public class MainActivity extends AppCompatActivity
             }
         }
 
+        //Makes sure that whatever button was clicked on is an actual empty box and doesn't have
+        //any symbols in it.
         if(tempButton.getText() == (""))
         {
+            //Checks what players turn it is and if the player has set a win condition yet.
             if(player == ("X"))
             {
                 tempButton.setText("X");
-                player = "O";
+                if(!winCond())
+                {
+                    player = "O";
+                }
             }
+            //Same as above but its for player O
             else
             {
                 tempButton.setText("O");
-                player = "X";
+                if(!winCond())
+                {
+                    player = "X";
+                }
             }
+            //Removed the button disable line here.
             //tempButton.setEnabled(false);
-
-            currTurn.setText("Player " + player + "'s turn");
+            count++;
+            //The count 5 is because 5 is the minimum amount of turns in order to win.
+            //X O X O X -> X wins for example.
+            if(count >= 5 && winCond())
+            {
+                currTurn.setText("Player " + player + " wins!");
+            }
+            //If no win then its other players turn.
+            else
+            {
+                currTurn.setText("Player " + player + "'s turn");
+            }
         }
 
+
+
+        //If tie is reached then the game is a deadlock and it will say tie game and prevent players
+        //from changing any values in the buttons.
+        if(count == tie)
+        {
+            currTurn.setText("Tie game!");
+        }
+
+        //If the view ever becomes newGame then it will reset all tiles to blank and become
+        //x's turn.
         if (v == newGameButton)
         {
             resetGame();
         }
+    }
+
+    //Simple method to determine if a winner has been selected yet or not.
+    private boolean winCond()
+    {
+        //Made a string array to help clean up the big if statement below.
+        String[] buttonArray = {buttonTL.getText().toString(),buttonTM.getText().toString(),buttonTR.getText().toString(),buttonML.getText().toString(),buttonMM.getText().toString(),
+                buttonMR.getText().toString(),buttonBL.getText().toString(),buttonBM.getText().toString(),buttonBR.getText().toString()};
+        Button[] buttons = {buttonTL,buttonTM,buttonTR,buttonML,buttonMM,buttonMR,buttonBL,buttonBM,buttonBR};
+        //A very ugly if statement that I was thinking of making a switch that basically runs through each of the 8 win conditions and then also checks
+        //that the spaces aren't blank for the win condition otherwise X would always win from the start.
+        if((buttonArray[0]== buttonArray[1] && buttonArray[1] == buttonArray[2]) && (buttonArray[0] != "" || buttonArray[1] != "" || buttonArray[2] != "") ||
+                (buttonArray[3] == buttonArray[4] && buttonArray[4] == buttonArray[5]) && (buttonArray[3] != "" || buttonArray[4] != "" || buttonArray[5] != "") ||
+                (buttonArray[6] == buttonArray[7] && buttonArray[7] == buttonArray[8]) && (buttonArray[6] != "" || buttonArray[7] != "" || buttonArray[8] != "") ||
+                (buttonArray[0] == buttonArray[3] && buttonArray[3] == buttonArray[6]) && (buttonArray[0] != "" || buttonArray[3] != "" || buttonArray[6] != "") ||
+                (buttonArray[1] == buttonArray[4] && buttonArray[4] == buttonArray[7]) && (buttonArray[1] != "" || buttonArray[4] != "" || buttonArray[7] != "") ||
+                (buttonArray[2] == buttonArray[5] && buttonArray[5] == buttonArray[8]) && (buttonArray[2] != "" || buttonArray[5] != "" || buttonArray[8] != "") ||
+                (buttonArray[0] == buttonArray[4] && buttonArray[4] == buttonArray[8]) && (buttonArray[0] != "" || buttonArray[4] != "" || buttonArray[8] != "") ||
+                (buttonArray[2] == buttonArray[4] && buttonArray[4] == buttonArray[6]) && (buttonArray[2] != "" || buttonArray[4] != "" || buttonArray[6] != ""))
+        {
+            //I disable the buttons here and not in the tie because if I don't the player who won is still allowed
+            //to fill out the board so this prevents that from happening.
+            for(int i = 0; i < buttons.length; i++)
+            {
+                buttons[i].setEnabled(false);
+            }
+            return true;
+        }
+        return false;
     }
 }
